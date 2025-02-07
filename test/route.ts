2a6a657f685f33c6ec493d79257924a09deb5ed2
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-02-06 21:26:28 -0800

    secret key

--- test/route.ts
+++ test/route.ts
@@ -1,6 +1,8 @@
 export function GET(request: Request) {
     const { searchParams } = new URL(request.url);
-    const dog = searchParams.get('cat');
+
+
+    const SUPER_SECRET_KEY = process.env.SUPER_SECRET_KEY;
     return Response.json({
         message: `Hello!`
     });
