b0a2633288258400c4e02529009ba3e07281fda8
Rhys Sullivan <39114868+RhysSullivan@users.noreply.github.com>
2025-02-06 21:26:09 -0800

    cat

--- test/route.ts
+++ test/route.ts
@@ -1,6 +1,6 @@
 export function GET(request: Request) {
     const { searchParams } = new URL(request.url);
-    const dog = searchParams.get('dog');
+    const dog = searchParams.get('cat');
     return Response.json({
         message: `Hello!`
     });
